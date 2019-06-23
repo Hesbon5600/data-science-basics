3a342b12e4ddba6b2dc37d1a6cb7edb331cd1d87
Hesbon5600 <hesbonkiptoo5600@gmail.com>
2019-06-23 16:52:08 +0300

    Data science course
    - basics of data science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A sample project to get a better understanding of data science',
+    author='Hesbon',
+    license='MIT',
+)
